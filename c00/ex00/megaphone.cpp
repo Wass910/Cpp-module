e3d412a0ccd8eb0ab5452ca1a9eaa891562bb021
wassim dhiba <wassimdhibaa@gmail.com>
2022-01-16 00:51:40 +0100

    need to do the last exercise

---
+++ c00/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string.h>
+
+
+int main(int argc, char **argv)
+{
+    int i = 1;
+    int count = 0;
+    char c;
+    
+    if (argc == 1) 
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    while (argv[i])
+    {
+        count = 0;
+        while (argv[i][count])
+        {
+            if (argv[i][count] >= 'a' && argv[i][count] <= 'z')
+            {
+                c = argv[i][count] - 32;
+                std::cout << c;
+            }
+            else
+                std::cout << argv[i][count];
+            count++;
+        }
+        i++;
+    }
+    std::cout <<  std::endl;
+    return 0;
+}
